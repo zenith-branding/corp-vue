f157f86e31eea3d2b701f4e85407813cf861d602
YURI SOROKINS <yuri@gudideas.co.uk>
2023-01-20 12:56:50 +0000

    Add navbar home

--- src/components/HeaderBottom.vue
+++ src/components/HeaderBottom.vue
@@ -2,6 +2,7 @@
   <div class="container-xl">
     <nav class="navbar navbar-expand-lg navbar-light bg-white">
       <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
         <button
           class="navbar-toggler"
           type="button"
