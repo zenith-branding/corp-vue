4f651eb03a43506618b110cd209e078c6238d520
Yuri Sorokins <93756242+yuridevweb@users.noreply.github.com>
2023-02-04 16:59:06 +0000

    Add date picker

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
 import store from "./store"
+// Date Picker
+import Datepicker from "@vuepic/vue-datepicker"
+import "@vuepic/vue-datepicker/dist/main.css"
 
 import { Bootstrap4Pagination } from "laravel-vue-pagination"
 
@@ -11,6 +14,7 @@ const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.component("Datepicker", Datepicker)
 app.component("Bootstrap4Pagination", Bootstrap4Pagination)
 
 app.mount("#app")
--- src/views/account/components/PersonalInformation.vue
+++ src/views/account/components/PersonalInformation.vue
@@ -2,57 +2,42 @@
   <div class="row button-row mb-3 p-1 pt-4">
     <div class="col-sm-12">
       <h4 class="card-title text-brand">General</h4>
-      <p class="card-text sub-heading  pb-3">Basic information about you</p>
-      
+      <p class="card-text sub-heading pb-3">Basic information about you</p>
+
       <!-- Title -->
       <div class="d-flex flex-row border-bottom">
         <label for="Title" class="account-label mt-2">Title:</label>
         <select class="custom-select mb-4" id="inlineFormCustomSelect">
-              <option selected disabled>Title</option>
-              <option value="Mr">Mr</option>
-              <option value="Miss">Miss</option>
-              <option value="Mrs">Mrs</option>
-              <option value="Ms">Ms</option>
-              <option value="Dr">Dr</option>
-              <option value="Mx">Mx</option>
-            </select>
+          <option selected disabled>Title</option>
+          <option value="Mr">Mr</option>
+          <option value="Miss">Miss</option>
+          <option value="Mrs">Mrs</option>
+          <option value="Ms">Ms</option>
+          <option value="Dr">Dr</option>
+          <option value="Mx">Mx</option>
+        </select>
       </div>
       <!-- Forename -->
       <div class="d-flex flex-row border-bottom">
         <label for="forename" class="account-label">Forename:</label>
-        <input
-          type="text"
-          class="form-control my-4"
-          placeholder="John"
-        />
+        <input type="text" class="form-control my-4" placeholder="John" />
       </div>
       <!-- Surname -->
       <div class="d-flex flex-row border-bottom">
         <label for="Surname" class="account-label">Surname:</label>
-        <input
-          type="text"
-          class="form-control my-4"
-          placeholder="Doe"
-        />
+        <input type="text" class="form-control my-4" placeholder="Doe" />
       </div>
 
       <!-- Date of Birth -->
       <div class="d-flex flex-row border-bottom">
         <label for="Date of Birth" class="account-label">Date of Birth:</label>
-        <input
-          type="text"
-          class="form-control my-4"
-          placeholder="info@mlg.co.uk"
-        />
-       
+
+        <Datepicker v-model="date" class=" my-4"></Datepicker>
       </div>
 
-
       <h4 class="card-title text-brand pb-3">Contact Numbers</h4>
-      <p class="card-text sub-heading">
-        Contact details
-      </p>
-            <!-- Email -->
+      <p class="card-text sub-heading">Contact details</p>
+      <!-- Email -->
       <div class="d-flex flex-row border-bottom">
         <label for="Email" class="account-label">Email:</label>
         <input
@@ -61,7 +46,7 @@
           placeholder="info@mlg.co.uk"
         />
       </div>
-            <!-- Mobile Phone -->
+      <!-- Mobile Phone -->
       <div class="d-flex flex-row border-bottom">
         <label for="Mobile Phone" class="account-label">Mobile Phone:</label>
         <input
@@ -75,10 +60,18 @@
 </template>
 
 <script>
+import Datepicker from "@vuepic/vue-datepicker"
+import "@vuepic/vue-datepicker/dist/main.css"
 
 export default {
   name: "PersonalInformation",
-    components: {
+  components: {
+    Datepicker,
+  },
+  data() {
+    return {
+      date: null,
+    }
   },
 }
 </script>
