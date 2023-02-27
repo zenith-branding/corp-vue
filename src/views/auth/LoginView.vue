ae1c7bc910e0d680d7ced25cacda865e41586d4a
YURI SOROKINS <yuri@gudideas.co.uk>
2023-02-27 14:48:01 +0000

    Refactor login page styles

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -3,45 +3,58 @@
     <div
       class="col-sm-6 p-5 vh-100 d-flex justify-content-center align-items-center"
     >
-      <div>
+      <div class="auth-container text-center">
+        
         <img
-          src="../../assets/mlg-sm-logo.png"
+          src="../../assets/mlg-login-logo.png"
           alt="Gud Ideas Corp"
-          class="logo"
+          class="logo mb-2"
         />
-        <h1 class="main-heading mb-3">Sign In.</h1>
+        <h1 class="main-heading mb-4">Sign Into Your Account</h1>
 
-        <p v-if="incorrectAuth" class="badge badge-warning">
+        <p v-if="incorrectAuth" class="badge badge-warning p-3 w-100 mb-3">
           Incorrect username or password entered!
-           </p>
+        </p>
         <form v-on:submit.prevent="login">
-          <div class="mb-3">
-            <label for="email" class="form-label">Email address</label>
+          <div class="mb-4">
+            <!-- <label for="email" class="form-label">Email address</label> -->
             <input
               type="email"
-              class="form-control"
+              class="form-control p-3"
               id="email"
               name="email"
               v-model="email"
               aria-describedby="emailHelp"
+              placeholder="Email address"
             />
-            <div id="emailHelp" class="form-text">
-              We'll never share your email with anyone else.
-            </div>
           </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label"
+          <div class="mb-4">
+            <!-- <label for="exampleInputPassword1" class="form-label"
               >Password</label
-            >
+            > -->
             <input
               type="password"
-              class="form-control"
+              class="form-control p-3"
               name="password"
               id="pass"
+              placeholder="Password"
               v-model="password"
             />
           </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <div class="form-auth-details mb-4">
+            <div class="form-check form-details-link" >
+              <input
+                type="checkbox"
+                class="checkbox"
+                id="exampleCheck1"
+              />
+              <label class="form-check-label" for="exampleCheck1"
+                >Remember me</label
+              >
+            </div>
+            <router-link class="form-details-link" to="/login" title="login">Forgot your password?</router-link>
+          </div>
+          <button type="submit" class="btn btn-primary w-100 p-3">Submit</button>
         </form>
       </div>
     </div>
@@ -51,7 +64,6 @@
 </template>
 
 <script>
-
 export default {
   name: "LoginView",
   data() {
